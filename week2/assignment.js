06e6876b4659aca89a2065e2c6b26f2c25e79caf
Min-su Kim <dlxhshzh@korea.ac.kr>
2018-10-14 13:34:34 +0900

    Promise

--- week2/assignment.js
+++ week2/assignment.js
@@ -25,20 +25,6 @@ function fs_readFile(fimeName, type)
     });
 }
 
-fs.readFile('./1.txt','UTF-8', function(err,data1) {
-    fs.writeFile('./2.txt',data1 + '22222', function(err) {
-        console.log('2.txt created');
-        fs.readFile('./2.txt','UTF-8', function(err,data2) {
-            fs.writeFile('./3.txt',data2 + '33333', function(err) {
-                console.log('3.txt created');
-                fs.readFile('./3.txt','UTF-8', function(err,data3) {
-                    console.log(data3);
-                });
-            });
-        });
-    });
-});
-
 fs_writeFile('./1.txt', '11111')
     .then(() => { return fs_readFile('./1.txt', "UTF-8") })
     .catch((err) => { console.log("Error!") })
