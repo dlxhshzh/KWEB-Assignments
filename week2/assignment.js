3176187b0b9f25b47017bcc797ca1b3240247a75
Min-su Kim <dlxhshzh@korea.ac.kr>
2018-10-31 20:52:06 +0900

    async await revision

--- week2/assignment.js
+++ week2/assignment.js
@@ -1,45 +1,26 @@
 const fs = require('fs');
+const util = require('util');
 
-function fs_writeFile(fileName, data)
+const fs_writeFile = util.promisify(fs.writeFile);
+const fs_readFile = util.promisify(fs.readFile);
+
+(async () => 
 {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(fileName, data, (err) => {
-            if (err)
-            {
-                reject(err);
-            }
-            else
-            {
-                console.log(fileName + " created.");
-                resolve(data);
-            }
-        });
-    });
-}
-function fs_readFile(fileName, type)
-{
-    return new Promise((resolve, reject) => {
-        fs.readFile(fileName, type, (err, data) => {
-            err ? reject(err) : resolve(data);
-        });
-    });
-}
-
-fs_writeFile('./1.txt', '11111')
-    .then(() => { return fs_readFile('./1.txt', "UTF-8") })
-    .catch((err) => { console.log("Error!") })
-
-    .then((data1) => { return fs_writeFile('./2.txt', data1 + '22222') })
-    .catch((err) => { console.log("Error") })
-    
-    .then(() => { return fs_readFile('./2.txt', 'UTF-8')})
-    .catch((err) => { console.log("Error") })
-
-    .then((data2) => { return fs_writeFile('./3.txt', data2 + '33333') })
-    .catch((err) => { console.log("Error") })
-
-    .then (() => { return fs_readFile('./3.txt', "UTF-8") })
-    .catch((err) => { console.log("Error") })
-
-    .then ((data3) => { console.log(data3) })
-    .catch((err) => { console.log("Error") });
+    try
+    {
+        await fs_writeFile('./1.txt', '11111');
+        console.log("1.txt just created");
+        const   data1 = await fs_readFile('./1.txt', "UTF-8");
+        await fs_writeFile('./2.txt', data1 + '22222');
+        console.log("2.txt just created");
+        const   data2 = await fs_readFile('./2.txt', "UTF-8");
+        await fs_writeFile('./3.txt', data2 + '33333');
+        console.log("3.txt just created");
+        const   data3 = await fs_readFile('./3.txt', "UTF-8");
+        console.log(data3);
+    }
+    catch(err)
+    {
+        console.log('Error');
+    }
+})();
